64626f752f0628a835838d47dce253995995b4fc
Sumitkumaryadav1210 <raghuyadav1210@gmail.com>
2026-08-26 13:58:00 +0530

    add-css-file

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link, NavLink } from 'react-router-dom'
+import './Navbar.css'
 
 export default function Navbar() {
   return (
@@ -11,6 +12,7 @@ export default function Navbar() {
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
+    
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav site-nav-links">
         <NavLink className="nav-link" to="/">Home</NavLink>
